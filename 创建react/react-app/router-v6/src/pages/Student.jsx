c9a033bd416025745f5d712c5c7b130c41f31a83
sx13694658890 <61036704+sx13694658890@users.noreply.github.com>
2022-07-21 23:31:48 +0800

    Delete 创建react/react-app/router-v6 directory

--- 创建react/react-app/router-v6/src/pages/Student.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-
-export default function Student() {
-    const {id}=useParams()
-  return (
-    <div>Student===={id}</div>
-  )
-}
